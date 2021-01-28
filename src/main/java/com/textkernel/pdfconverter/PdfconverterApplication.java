735c19d315b0257b4ce2bb5d16b9bc5c677da20c
Süleyman Gezsat <suleymangezsat@hotmail.com>
2021-01-28 16:21:51 +0300

    first commit

---
+++ src/main/java/com/textkernel/pdfconverter/PdfconverterApplication.java
@@ -0,0 +1,13 @@
+package com.textkernel.pdfconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfconverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfconverterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/textkernel/pdfconverter/PdfconverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.textkernel.pdfconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfconverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
